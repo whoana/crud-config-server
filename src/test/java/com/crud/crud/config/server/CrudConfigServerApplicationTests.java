5f256ffc4603cdab5826f11b935dc489fe8cc054
whoana <whoana@gmail.com>
2023-04-21 11:21:33 +0900

    first commit

---
+++ src/test/java/com/crud/crud/config/server/CrudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.crud.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
